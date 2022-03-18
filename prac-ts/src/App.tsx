2780e9a143dd2c5fc4e915d5ab8d98f4d96125e5
hyun <hyunsokstar@gmail.com>
2022-03-18 12:19:35 +0900

    first commit

---
+++ prac-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="red" bgColor="white" />
+      <Circle bgColor="skyblue" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-masterclass/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+function App() {
+    return (
+        <Wrapper>
+            <Title>Hello</Title>
+        </Wrapper>
+    );
+}
+
+export default App;
